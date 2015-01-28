e83df69d675eec01bf3253a2c7911cedb0c081af
Ian Weller <ianweller@buttslol.net>
2015-01-28 13:37:45 -0600

    Fix basic test case for Queryable class in Python 3

--- tests/test_queryable.py
+++ tests/test_queryable.py
@@ -1,7 +1,9 @@
 from busbus.queryable import Queryable
 
+from six.moves import range
+
 
 def test_queryable():
-    q = Queryable(xrange(10)).where(lambda x: x % 5 == 0)
+    q = Queryable(range(10)).where(lambda x: x % 5 == 0)
     assert next(q) == 0
     assert next(q) == 5
