4736ed07ea8b83ca8c32c2d675f67883050b8c26
Ian Weller <ianweller@buttslol.net>
2015-03-10 15:49:07 -0500

    Add a simple test case for LawrenceTransitProvider

---
+++ tests/test_provider_lawrenceks.py
@@ -0,0 +1,19 @@
+import busbus
+from busbus.provider.lawrenceks import LawrenceTransitProvider
+
+import arrow
+import pytest
+
+
+@pytest.fixture(scope='module')
+def lawrenceks_provider():
+    return LawrenceTransitProvider()
+
+
+def test_43_to_eaton_hall(lawrenceks_provider):
+    stop = lawrenceks_provider.get(busbus.Stop, u'15TH_SPAHR_WB')
+    route = lawrenceks_provider.get(busbus.Route, u'RT_43')
+    assert len(list(lawrenceks_provider.arrivals.where(
+        stop=stop, route=route,
+        start_time=arrow.get('2015-03-10T14:00:00-05:00'),
+        end_time=arrow.get('2015-03-10T16:00:00-05:00')))) == 13
