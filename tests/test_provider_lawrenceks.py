423c29e859440b4a774a924badb7da4417dbd5c6
Ian Weller <ianweller@buttslol.net>
2015-04-22 21:25:42 -0500

    Add another stop to test_43_to_eaton_hall

--- tests/test_provider_lawrenceks.py
+++ tests/test_provider_lawrenceks.py
@@ -15,10 +15,14 @@ def test_agency_phone_e164(lawrenceks_provider):
     assert agency.phone_e164 == '+17858644644'
 
 
-def test_43_to_eaton_hall(lawrenceks_provider):
-    stop = lawrenceks_provider.get(busbus.Stop, u'15TH_SPAHR_WB')
+@pytest.mark.parametrize('stop_id,count', [
+    (u'15TH_SPAHR_WB', 13),
+    (u'SNOW_HALL_WB', 14),
+])
+def test_43_to_eaton_hall(lawrenceks_provider, stop_id, count):
+    stop = lawrenceks_provider.get(busbus.Stop, stop_id)
     route = lawrenceks_provider.get(busbus.Route, u'RT_43')
     assert len(list(lawrenceks_provider.arrivals.where(
         stop=stop, route=route,
         start_time=arrow.get('2015-03-10T14:00:00-05:00'),
-        end_time=arrow.get('2015-03-10T16:00:00-05:00')))) == 13
+        end_time=arrow.get('2015-03-10T16:00:00-05:00')))) == count
