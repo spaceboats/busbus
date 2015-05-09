5d4cc8d27b175b2e078477cf6502e4f8d32ce3ae
Ian Weller <ianweller@buttslol.net>
2015-05-08 21:31:35 -0500

    Fetch missing test data on `setup.py test`

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 import collections
+import errno
 import os
 import sys
+import zipfile
 
 SETUP_DIR = os.path.abspath(os.path.dirname(__file__))
+VERSION = '0.1.0'
 
 
 class PyTest(TestCommand):
@@ -21,6 +24,8 @@ class PyTest(TestCommand):
         self.test_suite = True
 
     def run_tests(self):
+        fetch_test_data()
+
         import pytest
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
@@ -53,9 +58,52 @@ def get_requires(filename='requirements.txt'):
                 yield line
 
 
+def fetch_test_data():
+    """
+    Fetches test data if it's missing.
+    """
+
+    data_dir = os.path.join(SETUP_DIR, 'tests', 'data')
+
+    # if tests/data exists, is empty, and .gitmodules exists, run the
+    # appropriate git-submodule(1) command
+    if (os.path.isdir(data_dir) and not os.listdir(data_dir) and
+            os.path.isfile(os.path.join(SETUP_DIR, '.gitmodules'))):
+        import subprocess
+        subprocess.call(['git', 'submodule', 'update',
+                         '--init', '--recursive'])
+    # if tests/data either doesn't exist or is empty, download the
+    # busbus-test-data zip from GitHub and unpack it
+    else:
+        try:
+            os.mkdir(data_dir)
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+
+        if not os.listdir(data_dir):
+            # at this point requests and six are already installed
+            import requests
+            import six
+
+            resp = requests.get('https://github.com/spaceboats/busbus-test-'
+                                'data/archive/v{0}.zip'.format(VERSION))
+            with zipfile.ZipFile(six.BytesIO(resp.content)) as z:
+                prefix = 'busbus-test-data-{0}/'.format(VERSION)
+                for filename in z.namelist():
+                    if filename == prefix or not filename.startswith(prefix):
+                        continue
+                    filename = filename[len(prefix):]
+                    if filename.endswith('/'):
+                        os.mkdir(os.path.join(data_dir, filename))
+                    else:
+                        with open(os.path.join(data_dir, filename), 'wb') as f:
+                            f.write(z.read(prefix + filename))
+
+
 setup(
     name='busbus',
-    version='0.1.0',
+    version=VERSION,
     license='MIT',
     url='https://github.com/spaceboats/busbus',
     packages=find_packages(),
